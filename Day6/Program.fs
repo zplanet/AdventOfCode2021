cfbc5d01b58862eb9a81af261cc4600997e0fe0c
Jong Hoon Kim <zplanet@hotmail.com>
2021-12-09 08:20:59 -0500

    Day6 part two

--- Day6/Program.fs
+++ Day6/Program.fs
@@ -2,8 +2,9 @@
 
 open System
 open System.IO
+open System.Collections.Generic
+open System.Diagnostics
 
-// Define a function to construct a message to print
 let initialState = 
     Path.Combine(Directory.GetCurrentDirectory(), "input.txt")
     |> File.ReadLines
@@ -11,22 +12,54 @@ let initialState =
     |> (fun line -> Seq.toList (line.Split(',')))
     |> List.map (fun s -> Int32.Parse(s))
 
-let afterDays days state =
-    [ 1 .. days ]
-    |> List.fold 
-        (fun (s, nf) _ -> 
-            let ns = s |> List.map (fun x -> x - 1) |> List.map (fun x -> if x < 0 then 6 else x)
-            let zeros = ns |> List.filter (fun x -> 0 = x) |> List.length
-            (List.append ns [ for i in 1 .. nf -> 8 ], zeros))
-        (state, 0)
-    |> fst
+let afterDays timer days =
+    let cache = new Dictionary<_,_>()
+    let calcNewFishes days =
+        match cache.TryGetValue days with
+        | true, v -> v
+        | false, _ ->
+            let d = (days / 7) - (if 0 = days % 7 then 1 else 0)
+            let r = (9, days)::[ for i in 1 .. d -> (9, days - 7 * i)]
+            cache.Add(days, (List.rev r))
+            r
+    let rec iter r ls =
+        match ls with
+        | (tmr, ds)::tl when tmr < ds -> iter (r+(1UL)) (List.append (calcNewFishes (ds - tmr)) tl)
+        | (tmr, ds)::tl when ds <= tmr -> iter (r+(1UL)) tl
+        | _ -> r
+    iter (0UL) [(timer, days)]
+
+let runWithTimer f = 
+    let sw = new Stopwatch()
+    sw.Start()
+    f initialState
+    sw.Stop()
+    printfn "elapsed time: %A" (sw.Elapsed.ToString())
 
 [<EntryPoint>]
 let main argv =
+    
+    runWithTimer (fun state ->
+        state
+        |> List.map (fun timer -> afterDays timer 80)
+        |> List.sum
+        |> printfn "part one: %A"
+    )
 
-    initialState
-    |> afterDays 80
-    |> List.length
-    |> printfn "part one: %A"
+    runWithTimer (fun state ->
+        let cache = new Dictionary<_,_>()
 
+        state
+        |> List.map 
+            (fun timer -> 
+                match cache.TryGetValue timer with
+                | true, r -> r
+                | false, _ -> 
+                    let r = afterDays timer 256
+                    cache.Add(timer, r)
+                    r)
+        |> List.sum
+        |> printfn "part two: %A"
+    )
+    
     0 // return an integer exit code
